f2997fdba1a3f4c8e2f4978fa8f07e44d9b38c90
Johang727 <dev@johang.dev>
2026-05-29 21:37:12 -0500

    Added the ability to hide hacks from the list

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 from tkinterdnd2 import DND_FILES, TkinterDnD
-import os, hashlib, subprocess, re
+import os, hashlib, subprocess, shutil
 from tkinter import filedialog, messagebox
 
 HACKS_DIR:str = "./hacks"
@@ -11,6 +11,46 @@ PATCHED_DIR:str = "./patched"
 
 US_CLEAN_HASH:str = "10af6a1a4d90ffb48bfb6c444324f7fd"
 EU_CLEAN_HASH:str = "6735749e060e002efd88e61560e45567"
+
+def toggle_complete() -> None:
+    try:
+        selected_index = hack_listbox.curselection()[0]
+        selected_text = hack_listbox.get(selected_index)
+
+        filename:str = selected_text.strip().removeprefix("✅ ")
+
+
+        if filename.endswith(".nds"):
+            is_patched:bool = True
+        else:
+            is_patched = False
+
+        filename = filename.removesuffix(".xdelta").removesuffix(".nds")
+
+
+        if filename.startswith("HIDDEN_"):
+            # file is hidden, show it
+            filename = filename.removeprefix("HIDDEN_")
+            if is_patched:
+                shutil.move(f"{PATCHED_DIR}/HIDDEN_{filename}.nds", f"{PATCHED_DIR}/{filename}.nds")
+                if os.path.isfile(f"{HACKS_DIR}/HIDDEN_{filename}.xdelta"):
+                    shutil.move(f"{HACKS_DIR}/HIDDEN_{filename}.xdelta", f"{HACKS_DIR}/{filename}.xdelta")
+            else:
+                shutil.move(f"{HACKS_DIR}/HIDDEN_{filename}.xdelta", f"{HACKS_DIR}/{filename}.xdelta")
+        else:
+            # file is shown, hide it
+            if is_patched:
+                shutil.move(f"{PATCHED_DIR}/{filename}.nds", f"{PATCHED_DIR}/HIDDEN_{filename}.nds")
+                if os.path.isfile(f"{HACKS_DIR}/{filename}.xdelta"):
+                    shutil.move(f"{HACKS_DIR}/{filename}.xdelta", f"{HACKS_DIR}/HIDDEN_{filename}.xdelta")
+            else:
+                shutil.move(f"{HACKS_DIR}/{filename}.xdelta", f"{HACKS_DIR}/HIDDEN_{filename}.xdelta")
+
+
+        refresh_hack_list()
+
+    except IndexError:
+        messagebox.showwarning("Warning", "Please select a hack from the list first.")
 
 def select_clean_rom() -> None:
     directory = filedialog.askdirectory()
@@ -82,8 +122,10 @@ def auto_detect_roms() -> None:
 def refresh_hack_list():
     if not os.path.exists(HACKS_DIR):
         os.makedirs(HACKS_DIR)
+
     if not os.path.exists(PATCHED_DIR):
         os.makedirs(PATCHED_DIR)
+
 
     hack_listbox.delete(0, tk.END)
 
@@ -91,7 +133,9 @@ def refresh_hack_list():
         files = [f for f in os.listdir(PATCHED_DIR) if f.endswith('.nds')]
 
         for file in files:
-            hack_listbox.insert(tk.END, f" ✅ {file}")
+            if show_hidden.get() or not file.startswith("HIDDEN_"):
+                hack_listbox.insert(tk.END, f" ✅ {file}")
+
 
     except Exception as e:
         messagebox.showerror("Error", f"Could not read patched folder: {e}")
@@ -106,7 +150,8 @@ def refresh_hack_list():
         for file in files:
             raw_name = file.removesuffix(".xdelta")
             if f" ✅ {raw_name}.nds" not in hack_listbox.get(0, tk.END):
-                hack_listbox.insert(tk.END, f" 📄 {file}")
+                if show_hidden.get() or not file.startswith("HIDDEN_"):
+                    hack_listbox.insert(tk.END, f"{ file}")
     except Exception as e:
         messagebox.showerror("Error", f"Could not read hacks folder: {e}")
 
@@ -115,12 +160,16 @@ def get_selected_hack():
     try:
         selected_index = hack_listbox.curselection()[0]
         selected_text = hack_listbox.get(selected_index)
+
+        #print(selected_text)
         
         # Clean up the emoji prefix to get the raw filename
-        filename = selected_text.replace(" 📄 ", "")
-        filename = selected_text.replace(" ✅ ", "")
+
+        filename = selected_text.strip().removeprefix("✅ ")
+
+        #print(f"Should be clean: {filename}")
         
-        if re.match(".*nds", filename):
+        if filename.endswith(".nds"):
             play_hack(filename)
         else:
             apply_patch(filename)
@@ -137,7 +186,7 @@ def handle_drop(event):
 
     try:
         if hack_name.endswith(".xdelta"):
-            subprocess.run(["mv", filepath, f"{HACKS_DIR}/{hack_name}"])
+            shutil.move(filepath, f"{HACKS_DIR}/{hack_name}")
             refresh_hack_list()
             messagebox.showinfo("Success!", "Added to unpatched roms directory sucessfully!")
         elif hack_name.endswith(".nds"):
@@ -145,11 +194,11 @@ def handle_drop(event):
                 digest = hashlib.file_digest(f, "md5")
                 md5_hash = digest.hexdigest()
             if US_CLEAN_HASH == md5_hash or EU_CLEAN_HASH == md5_hash:
-                subprocess.run(["mv", filepath, f"{BASE_DIR}/{hack_name}"])
+                shutil.move(filepath, f"{BASE_DIR}/{hack_name}")
                 auto_detect_roms()
                 messagebox.showinfo("Success!", "Added to base roms directory sucessfully!")
             else:
-                subprocess.run(["mv", filepath, f"{PATCHED_DIR}/{hack_name}"])
+                shutil.move(filepath,f"{PATCHED_DIR}/{hack_name}" )
                 refresh_hack_list()
                 messagebox.showinfo("Success!", "Added to patched roms directory sucessfully!")
         else:
@@ -174,6 +223,9 @@ eu_rom_path:str = ""
 region = tk.StringVar()
 region.set("US")
 
+show_hidden = tk.BooleanVar()
+show_hidden.set(False)
+
 base_roms = tk.Frame(root)
 base_roms.pack()
 
@@ -189,32 +241,42 @@ eu_label.pack(side="right")
 
 auto_detect_roms()
 
-region_switcher = tk.Frame(root)
-region_switcher.pack(side="right", padx=(0, 20))
-
-
-us_btn = tk.Radiobutton(region_switcher, text="US", variable=region, value="US")
-us_btn.pack(side="top")
-eu_btn = tk.Radiobutton(region_switcher, text="EU", variable=region, value="EU")
-eu_btn.pack(side="top")
-
 list_label = tk.Label(root, text="Available Hacks:", font=("Hack", 10, "bold"))
 list_label.pack(pady=(10, 2), anchor="w", padx=10)
 
 list_frame = tk.Frame(root)
-list_frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=5)
+list_frame.pack(fill="both", expand=True, padx=20, pady=5)
 
 scrollbar = tk.Scrollbar(list_frame)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
+scrollbar.pack(side="right", fill="y")
 
 hack_listbox = tk.Listbox(list_frame, yscrollcommand=scrollbar.set, font=("Courier", 10), exportselection=False)
-hack_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+hack_listbox.pack(side="left", fill="both", expand=True)
+
+region_switcher = tk.Frame(root)
+region_switcher.pack(padx=(0, 0))
+
+us_btn = tk.Radiobutton(region_switcher, text="US", variable=region, value="US")
+us_btn.pack(side="left")
+eu_btn = tk.Radiobutton(region_switcher, text="EU", variable=region, value="EU")
+eu_btn.pack(side="left")
+
+show_hidden_box = tk.Checkbutton(region_switcher, text="Show Hidden:", command=refresh_hack_list, variable=show_hidden)
+show_hidden_box.pack(side="right")
+
 
 scrollbar.config(command=hack_listbox.yview)
 
-select_button = tk.Button(root, text="Play Hack", command=get_selected_hack)
-select_button.pack(pady=10)
+action_buttons = tk.Frame(root)
+action_buttons.pack(pady=10, side="bottom")
+
+
+select_button = tk.Button(action_buttons, text="Play Hack", command=get_selected_hack)
+select_button.pack(side="left")
+
+complete_button = tk.Button(action_buttons, text="Hide/Show", command=toggle_complete)
+complete_button.pack(side="right")
+
 
 refresh_hack_list()
 
